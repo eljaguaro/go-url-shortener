552c76c2775249ef0e957bf7577cdd8e4090c325
Давид Заде <Davidzade21@mail.ru>
2024-10-17 16:51:37 +0300

    incr6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,23 +7,23 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
 	"github.com/thanhpk/randstr"
+	"go.uber.org/zap"
 )
 
 var urlmap = make(map[string]string)
 
 func makeshortFunc(long string) string {
-	// return "fewe"
 	short := randstr.String(8)
 	urlmap[short] = long
 	return short
 }
 
 func geturlFunc(url string) string {
-	// return "fewef"
 	long, ok := urlmap[url]
 	if !ok {
 		return "The short url not found"
@@ -32,20 +32,12 @@ func geturlFunc(url string) string {
 }
 
 func geturlHandle(rw http.ResponseWriter, r *http.Request) {
-	// if r.Host != "localhost:8080" {
-	// 	rw.WriteHeader(http.StatusBadRequest)
-	// 	return
-	// }
 	long := geturlFunc(chi.URLParam(r, "id"))
 	if long == "The short url not found" {
 		rw.WriteHeader(http.StatusNotFound)
 		return
 	}
-	// rw.WriteHeader(http.StatusTemporaryRedirect)
-	// rw.Header().Set("Location", long)
-	// http.Redirect(rw, r, long, http.StatusPermanentRedirect)
 	http.Redirect(rw, r, long, http.StatusTemporaryRedirect)
-	// rw.Write([]byte("few"))
 }
 
 type URL struct {
@@ -53,10 +45,6 @@ type URL struct {
 }
 
 func makeshortHandle(rw http.ResponseWriter, r *http.Request, surladdr string) {
-	// if r.Host != "localhost:8080" {
-	// 	rw.WriteHeader(http.StatusBadRequest)
-	// 	return
-	// }
 	var url URL
 	b, err := io.ReadAll(r.Body)
 	url.URL = string(b)
@@ -65,33 +53,91 @@ func makeshortHandle(rw http.ResponseWriter, r *http.Request, surladdr string) {
 		rw.Write([]byte(err.Error()))
 		return
 	}
-	// http.RedirectHandler()
 	rw.WriteHeader(http.StatusCreated)
 	rw.Header().Set("Content-Type", "text/plain")
 	rw.Write([]byte(surladdr + "/" + makeshortFunc(url.URL)))
 }
 
-// func rhandle(rw http.ResponseWriter, r *http.Request) {
-// 	http.Redirect(rw, r, "http://abc.ru/", 309)
+func WithLogging(h http.HandlerFunc) http.HandlerFunc {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
 
-// }
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w, // встраиваем оригинальный http.ResponseWriter
+			responseData:   responseData,
+		}
+		h.ServeHTTP(&lw, r) // внедряем реализацию http.ResponseWriter
 
-// curl -X POST 'http://localhost:8080/' -H "text/plain" -d '{"URL": "abc"}'
-// var emptystr = ""
+		duration := time.Since(start)
+
+		sugar.Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"duration", duration,
+			"size", responseData.size, // получаем перехваченный размер ответа
+		)
+	}
+	return http.HandlerFunc(logFn)
+}
 
 type Config struct {
 	serverAddress *string `env:"SERVER_ADDRESS"`
 	baseURL       *string `env:"BASE_URL"`
 }
 
+var sugar zap.SugaredLogger
+
+type ResponseWriter interface {
+	Header() http.Header
+	Write([]byte) (int, error)
+	WriteHeader(statusCode int)
+}
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
 func main() {
-	// urlmap := make(map[string]string)
-	// r.Get("/fw", rhandle)
 	var cfg Config
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	sugar = *logger.Sugar()
+
 	run := flag.String("a", "localhost:8080", "адрес запуска http-сервера")
 	surladdr := flag.String("b", "http://localhost:8080", "базовый адрес результирующего URL")
 	flag.Parse()
@@ -105,81 +151,16 @@ func main() {
 	}
 	port := strings.Split(*run, ":")[1]
 	r := chi.NewRouter()
-	r.Post("/", func(rw http.ResponseWriter, r *http.Request) { makeshortHandle(rw, r, *surladdr) })
-	r.Get("/{id}", geturlHandle)
+	r.Post("/", WithLogging((func(rw http.ResponseWriter, r *http.Request) { makeshortHandle(rw, r, *surladdr) })))
+	r.Get("/{id}", WithLogging(geturlHandle))
+
+	addr := "127.0.0.1:8080"
+	sugar.Infow(
+		"Starting server",
+		"addr", addr,
+	)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		sugar.Fatalw(err.Error(), "event", "start server")
+	}
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
-
-// package main
-
-// import (
-// 	"encoding/json"
-// 	"io"
-// 	"log"
-// 	"net/http"
-
-// 	"github.com/go-chi/chi/v5"
-// 	"github.com/thanhpk/randstr"
-// )
-
-// var urlmap = make(map[string]string)
-
-// func makeshortFunc(long string) string {
-// 	// return "fewe"
-// 	short := randstr.String(8)
-// 	urlmap[short] = long
-// 	return short
-// }
-
-// func geturlFunc(url string) string {
-// 	// return "fewef"
-// 	long, ok := urlmap[url]
-// 	if !ok {
-// 		return "The short url not found"
-// 	}
-// 	return long
-// }
-
-// func geturlHandle(rw http.ResponseWriter, r *http.Request) {
-// 	long := geturlFunc(chi.URLParam(r, "id"))
-// 	if long == "The short url not found" {
-// 		rw.WriteHeader(http.StatusNotFound)
-// 		return
-// 	}
-// 	http.Redirect(rw, r, long, http.StatusTemporaryRedirect)
-// 	// rw.WriteHeader(http.StatusTemporaryRedirect)
-// 	io.WriteString(rw, long)
-// }
-
-// type URL struct {
-// 	URL string `json:"url"`
-// }
-
-// func makeshortHandle(rw http.ResponseWriter, r *http.Request) {
-// 	if r.Host != "localhost:8080" {
-// 		rw.WriteHeader(http.StatusBadRequest)
-// 		return
-// 	}
-// 	var url URL
-// 	err := json.NewDecoder(r.Body).Decode(&url)
-// 	if err != nil {
-// 		rw.WriteHeader(http.StatusBadRequest)
-// 		rw.Write([]byte(err.Error()))
-// 		return
-// 	}
-
-// 	rw.WriteHeader(http.StatusCreated)
-// 	rw.Write([]byte("http://localhost:8080/" + makeshortFunc(url.URL)))
-// }
-
-// // curl -X POST 'http://localhost:8080/' -H "text/plain" -d '{"URL": "abc"}'
-
-// func main() {
-// 	// urlmap := make(map[string]string)
-// 	r := chi.NewRouter()
-// 	r.Post("/", makeshortHandle)
-// 	r.Get("/{id}", geturlHandle)
-// 	log.Fatal(http.ListenAndServe(":8080", r))
-// }
-
-// // k0CA1T3m
--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/thanhpk/randstr v1.0.6 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/net v0.27.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
